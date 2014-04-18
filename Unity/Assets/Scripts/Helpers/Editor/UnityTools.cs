48186b713a396de5c1d869bf72dfaaacd65897ca
Lucky Bremachandra <lucky@massdmg.com>
2014-04-18 12:26:52 -0400

    Created Unity helper tools.

---
+++ Unity/Assets/Scripts/Helpers/Editor/UnityTools.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class UnityTools
+{
+	#region Clear PlayerPrefs
+	
+	[MenuItem("Tools/Clear PlayerPrefs")]
+	private static void ClearPlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.Save();
+		Debug.Log("Cleared PlayerPrefs.");
+	}
+	
+	#endregion
+}
